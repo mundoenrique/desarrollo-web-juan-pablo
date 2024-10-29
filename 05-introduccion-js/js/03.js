def5952cb8dba6e57a39d7282b7e50064d0de739
mundoenrique <enrique.penaloza.working@gmail.com>
2024-10-29 14:14:42 -0400

    Constantes

---
+++ 05-introduccion-js/js/03.js
@@ -0,0 +1,16 @@
+// Constantes
+
+const producto = 'Audifonos Gamer'; // Iniciar variable y asignarle valor
+
+const producto1 = 'Computadora',
+  disponible1 = true,
+  categoria = 'Computadoras';
+
+// const 1disponible;
+const disponible_ = true;
+
+// Estilos para las variables
+const nombre_producto = 'Monitor HD'; // underscore
+const nombreProducto = 'Monitor HD'; // Camelcase
+const NombreProducto = 'Monitor HD'; // Pascal Case
+const nombreproducto = 'MonitorHD';
