80bec3d63093d4cc30790ea9b0118e44fab9fd25
mundoenrique <enrique.penaloza.working@gmail.com>
2024-11-08 17:52:46 -0400

    querySelector

---
+++ 06-javascript-navegador/js/scripts.js
@@ -0,0 +1,8 @@
+// Seleccionar elementos...
+
+// querySelector
+
+const textoHeading = document.querySelector('.header__texto h2');
+console.log(textoHeading);
+
+textoHeading.textContent = 'Nuevo Heading'; // También se puede utilizar .text
