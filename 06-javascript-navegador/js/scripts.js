b982a3257c8a80019fdb66093c9f89bd6470ad63
mundoenrique <enrique.penaloza.working@gmail.com>
2024-11-08 18:06:25 -0400

    createElement

--- 06-javascript-navegador/js/scripts.js
+++ 06-javascript-navegador/js/scripts.js
@@ -24,3 +24,26 @@ enlaces[0].classList.add('nueva-clase');
 
 // Eliminar una clase...
 // enlaces[0].classList.remove('navegacion__enlace');
+
+// Generar HTML desde JavaScript...
+const nuevoEnlace = document.createElement('A');
+
+console.log(nuevoEnlace);
+
+// Un enlace tiene una clase...
+nuevoEnlace.classList.add('navegacion__enlace');
+
+// Tiene un href
+nuevoEnlace.href = 'nuevo-enlace.html';
+
+// Tiene un Texto...
+nuevoEnlace.textContent = 'Un Nuevo Enlace';
+
+// console.log(nuevoEnlace);
+
+// Finalmente se agrega donde lo deseamos colocar...
+
+// Tienes que seleccionar el elemento padre
+
+const navegacion = document.querySelector('.navegacion');
+navegacion.appendChild(nuevoEnlace);
