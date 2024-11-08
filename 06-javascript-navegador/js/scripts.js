16eb41cefbfb4837566b83314369a7c2c809dc92
mundoenrique <enrique.penaloza.working@gmail.com>
2024-11-08 18:21:02 -0400

    Eventos

--- 06-javascript-navegador/js/scripts.js
+++ 06-javascript-navegador/js/scripts.js
@@ -47,3 +47,33 @@ nuevoEnlace.textContent = 'Un Nuevo Enlace';
 
 const navegacion = document.querySelector('.navegacion');
 navegacion.appendChild(nuevoEnlace);
+
+// Eventos en JavaScript...
+
+// Hay muchos eventos ocurriendo en tus sitios y aplicaciones web, cuando un usuario da click, cuando dan scroll, al presionar en un botón, enviar un formulario, pero uno de los más comunes es esperar a que el documento este listo...
+
+console.log('1');
+window.addEventListener('load', function () {
+  // Cuando el Archivo JS y los archivos dependientes han cargado como es el HTML y las imagenes...
+  console.log('2');
+});
+
+window.onload = function () {
+  console.log('3');
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Este se ejecuta cuando el HTML ha sido descargado pero no espera por CSS o imagenes...
+  console.log('4');
+});
+
+console.log('5');
+
+// Estos closures también pueden ser con una función aparte...
+
+// Eventos Scroll...
+// window.onscroll = function (e) {
+//   console.log('scrolling...');
+
+//   console.log(this.scrollY);
+// };
