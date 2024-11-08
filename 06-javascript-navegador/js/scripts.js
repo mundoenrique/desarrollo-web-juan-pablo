77fff8ada9c7424b9540e5d2ef9688d8ea0f45b1
mundoenrique <enrique.penaloza.working@gmail.com>
2024-11-08 17:55:03 -0400

    querySelectorAll

--- 06-javascript-navegador/js/scripts.js
+++ 06-javascript-navegador/js/scripts.js
@@ -6,3 +6,21 @@ const textoHeading = document.querySelector('.header__texto h2');
 console.log(textoHeading);
 
 textoHeading.textContent = 'Nuevo Heading'; // También se puede utilizar .text
+
+// querySelectorAll
+const enlaces = document.querySelectorAll('.navegacion a');
+console.log(enlaces);
+
+// Algunas operaciones...
+
+// Cambiar el texto
+enlaces[0].textContent = 'Nuevo Texto enlace';
+
+// Cambiar el enlace del primer enlace
+enlaces[0].href = 'google.com';
+
+// Agregar una clase...
+enlaces[0].classList.add('nueva-clase');
+
+// Eliminar una clase...
+// enlaces[0].classList.remove('navegacion__enlace');
