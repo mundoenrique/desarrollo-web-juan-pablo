946211ec2f353d214e2519be2a16e971c698e210
mundoenrique <enrique.penaloza.working@gmail.com>
2024-11-09 14:06:56 -0400

    Genera css con gulp

--- 07-festival-musica-sass/gulpfile.js
+++ 07-festival-musica-sass/gulpfile.js
@@ -1,5 +1,10 @@
-export function hola(done) {
-  console.log('Hola mundo');
+import { src, dest } from 'gulp';
+import * as dartSass from 'sass';
+import gulpSass from 'gulp-sass';
 
+const sass = gulpSass(dartSass);
+
+export function css(done) {
+  src('src/scss/app.scss').pipe(sass()).pipe(dest('build/css'));
   done();
 }
