0ea93c0882314bab69145e6e95fb9e299830684b
mundoenrique <enrique.penaloza.working@gmail.com>
2024-11-09 14:54:19 -0400

    Escucha cambios en app.scss

--- 07-festival-musica-sass/gulpfile.js
+++ 07-festival-musica-sass/gulpfile.js
@@ -1,4 +1,4 @@
-import { src, dest } from 'gulp';
+import { src, dest, watch } from 'gulp';
 import * as dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 
@@ -7,4 +7,8 @@ const sass = gulpSass(dartSass);
 export function css(done) {
   src('src/scss/app.scss').pipe(sass()).pipe(dest('build/css'));
   done();
+}
+
+export function dev() {
+  watch('src/scss/app.scss', css);
 }
