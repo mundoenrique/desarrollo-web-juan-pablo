2b8d88b6aabc53370ea617bb75a988e15332223b
mundoenrique <enrique.penaloza.working@gmail.com>
2024-11-11 14:44:21 -0400

    minfica css y js

--- 08-festival-musica-performance/gulpfile.js
+++ 08-festival-musica-performance/gulpfile.js
@@ -1,11 +1,12 @@
 import { src, dest, watch, series } from 'gulp';
 import * as dartSass from 'sass';
 import gulpSass from 'gulp-sass';
+import terser from 'gulp-terser';
 
 const sass = gulpSass(dartSass);
 
 export function js(done) {
-  src('src/js/app.js').pipe(dest('build/js'));
+  src('src/js/app.js').pipe(terser()).pipe(dest('build/js'));
 
   done();
 }
@@ -14,7 +15,7 @@ export function css(done) {
   // const result = dartSass.compile('src/scss/app.scss', { style: 'compressed' });
   // console.log(result.css);
   src('src/scss/app.scss', { sourcemaps: true })
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(dest('build/css', { sourcemaps: true }));
   // .pipe(dest('build/css', { sourcemaps: '.' }));
   done();
