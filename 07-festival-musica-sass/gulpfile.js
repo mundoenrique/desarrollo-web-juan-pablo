063efd3a68c65ec8e94d59acbf42b34bf1bea78b
mundoenrique <enrique.penaloza.working@gmail.com>
2024-11-11 11:37:55 -0400

    Gulp para archivos js

--- 07-festival-musica-sass/gulpfile.js
+++ 07-festival-musica-sass/gulpfile.js
@@ -1,8 +1,14 @@
-import { src, dest, watch } from 'gulp';
+import { src, dest, watch, series } from 'gulp';
 import * as dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 
 const sass = gulpSass(dartSass);
+
+export function js(done) {
+  src('src/js/app.js').pipe(dest('build/js'));
+
+  done();
+}
 
 export function css(done) {
   // const result = dartSass.compile('src/scss/app.scss', { style: 'compressed' });
@@ -17,3 +23,5 @@ export function css(done) {
 export function dev() {
   watch('src/scss/**/*.scss', css);
 }
+
+export default series(js, css, dev);
