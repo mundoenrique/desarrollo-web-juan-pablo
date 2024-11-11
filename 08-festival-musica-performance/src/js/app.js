86eef4eac040dace44078840eefbb3048eea0a18
mundoenrique <enrique.penaloza.working@gmail.com>
2024-11-11 14:59:25 -0400

    carga retardada de imagenes

--- 08-festival-musica-performance/src/js/app.js
+++ 08-festival-musica-performance/src/js/app.js
@@ -24,6 +24,9 @@ function crearGaleria() {
 
   for (let i = 1; i <= CANTIDAD_IMAGENES; i++) {
     const imagen = document.createElement('IMG');
+    imagen.loading = 'lazy';
+    imagen.width = '300';
+    imagen.height = '200';
     imagen.src = `src/img/gallery/full/${i}.jpg`;
     imagen.alt = 'Galeria';
 
