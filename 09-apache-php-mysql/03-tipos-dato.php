fb984bc1cff4c27d06b4d7fc2f9b9d935f9af38f
mundoenrique <enrique.penaloza.working@gmail.com>
2024-11-12 12:17:04 -0400

    Tipos de datos

--- 09-apache-php-mysql/03-tipos-dato.php
+++ 09-apache-php-mysql/03-tipos-dato.php
@@ -1,5 +1,24 @@
-<?php include 'includes/header.php';
+<?php
+include 'includes/header.php';
+// Bollean
+$loguado = true;
+var_dump($loguado);
 
+// Entrero
+$numero = 200;
+var_dump($numero);
+
+// Floats
+$float = 200.5;
+var_dump($float);
+
+// String
+$nombre = 'Enrique';
+var_dump($nombre);
+
+// array
+$array = [];
+var_dump($array);
 
 
 
