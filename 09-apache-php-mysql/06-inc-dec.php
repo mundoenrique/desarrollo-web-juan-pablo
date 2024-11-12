c4d41ea31a7b6abe5611d116b4ef5a216396fa9c
mundoenrique <enrique.penaloza.working@gmail.com>
2024-11-12 12:40:03 -0400

    incremento y decremaneto

--- 09-apache-php-mysql/06-inc-dec.php
+++ 09-apache-php-mysql/06-inc-dec.php
@@ -1,6 +1,17 @@
-<?php include 'includes/header.php';
+<?php
+include 'includes/header.php';
 
+$numero1 = 30;
+echo ++$numero1;
 
+echo '<br>';
 
+$numero1 += 5;
+echo $numero1;
+
+echo '<br>';
+
+$numero2 = 30;
+echo --$numero2;
 
 include 'includes/footer.php';
