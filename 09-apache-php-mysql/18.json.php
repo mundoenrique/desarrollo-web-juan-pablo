2936ef72494c34b988cb709d45741b7cd10818e2
mundoenrique <enrique.penaloza.working@gmail.com>
2024-11-12 14:47:30 -0400

    Manejo de JSON

--- 09-apache-php-mysql/18.json.php
+++ 09-apache-php-mysql/18.json.php
@@ -1,8 +1,31 @@
-<?php include 'includes/header.php';
+<?php
+include 'includes/header.php';
 
+$productos = [
+  [
+    'nombre' => 'Tablet',
+    'precio' => 200,
+    'disponible' => true
+  ],
+  [
+    'nombre' => 'Televisión 24"',
+    'precio' => 300,
+    'disponible' => true
+  ],
+  [
+    'nombre' => 'Monitor Curvo /',
+    'precio' => 400,
+    'disponible' => false
+  ]
+];
 
+$json = json_encode($productos, JSON_UNESCAPED_UNICODE);
+$json_array = json_decode($json);
 
-
-
+echo "<pre>";
+var_dump($productos);
+var_dump($json);
+var_dump($json_array);
+echo "</pre>";
 
 include 'includes/footer.php';
