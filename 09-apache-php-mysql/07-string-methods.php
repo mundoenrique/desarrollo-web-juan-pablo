4363370abaab4998ad5d81e51271617b4d8ced4f
mundoenrique <enrique.penaloza.working@gmail.com>
2024-11-12 12:53:31 -0400

    Manejo de cadenas de texto

--- 09-apache-php-mysql/07-string-methods.php
+++ 09-apache-php-mysql/07-string-methods.php
@@ -1,6 +1,46 @@
-<?php include 'includes/header.php';
+<?php
+include 'includes/header.php';
 
+$nombreCliente = "Juan Pablo";
 
+// Conocer extension de un string
+echo strlen($nombreCliente);
+echo "<br>";
+var_dump($nombreCliente);
+echo "<br>";
 
+// Eliminar espacios en blanco
+$texto = trim($nombreCliente);
+echo strlen($texto);
+echo "<br>";
+
+//Convertirlo a mayusculas
+echo strtoupper($nombreCliente);
+echo "<br>";
+
+// Convertirlo en minusculas
+echo strtolower($nombreCliente);
+echo "<br>";
+
+$mail1 = "correo@correo.com";
+$mail2 = "Correo@correo.com";
+
+var_dump($mail1 === $mail2);
+echo "<br>";
+var_dump(strtolower($mail1) === strtolower($mail2));
+echo "<br>";
+echo str_replace('Juan', 'J', $nombreCliente);
+echo "<br>";
+
+// Revisar si un string existe o no
+echo strpos($nombreCliente, 'Juan');
+echo "<br>";
+
+$tipoCliente = "Premium";
+
+echo "<br>";
+echo "El Cliente " . $nombreCliente . " es " . $tipoCliente;
+echo "<br>";
+echo "El Cliente {$nombreCliente} es ${tipoCliente} ";
 
 include 'includes/footer.php';
