89821778396ae250eb4c0f8d39df91ec9b2842ec
mundoenrique <enrique.penaloza.working@gmail.com>
2024-11-12 12:26:10 -0400

    Operadores

--- 09-apache-php-mysql/04-operadores.php
+++ 09-apache-php-mysql/04-operadores.php
@@ -1,6 +1,28 @@
-<?php include 'includes/header.php';
+<?php
+include 'includes/header.php';
 
+$numero1 = 20;
+$numero2 = 10;
 
+// Sumar
+echo $numero1 + $numero2;
 
+echo "<br>";
+
+// Resta
+echo $numero1 - $numero2;
+
+echo "<br>";
+
+// Multiplicar
+echo $numero1 * $numero2;
+
+echo "<br>";
+
+// Dividir
+echo $numero1 / $numero2;
+
+// Multiplicar cierta cantidad de veces
+echo 2 ** 3;
 
 include 'includes/footer.php';
