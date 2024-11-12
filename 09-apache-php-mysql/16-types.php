46ca03894eff39386d7a1c058a29171d710aa564
mundoenrique <enrique.penaloza.working@gmail.com>
2024-11-12 14:29:13 -0400

    Retorno de datps y tipo

--- 09-apache-php-mysql/16-types.php
+++ 09-apache-php-mysql/16-types.php
@@ -1,7 +1,18 @@
-<?php include 'includes/header.php';
+<?php
 
+declare(strict_types=1);
+include 'includes/header.php';
 
+function usuarioAutenticado(bool $autenticado): ?string
+{
+  if ($autenticado) {
+    return "El Usuario esta autenticado";
+  } else {
+    return null;
+  }
+}
 
-
+$usuario = usuarioAutenticado(true);
+echo $usuario;
 
 include 'includes/footer.php';
