b5600e16de32a9c0a480cbcf197ad19d6ffee46a
mundoenrique <enrique.penaloza.working@gmail.com>
2024-11-12 14:06:50 -0400

    Ejemplo practico foreach

--- 09-apache-php-mysql/14-foreach.php
+++ 09-apache-php-mysql/14-foreach.php
@@ -1,6 +1,30 @@
 <?php include 'includes/header.php';
 
+$productos = [
+  [
+    'nombre' => 'Tablet',
+    'precio' => 200,
+    'disponible' => true
+  ],
+  [
+    'nombre' => 'Television 24"',
+    'precio' => 300,
+    'disponible' => true
+  ],
+  [
+    'nombre' => 'Monitor Curvo',
+    'precio' => 400,
+    'disponible' => false
+  ]
+];
 
-
+foreach ($productos as $producto) { ?>
+  <li>
+    <p>Producto: <?= $producto['nombre']; ?> </p>
+    <p>Precio: <?= "$" . $producto['precio']; ?> </p>
+    <p><?= ($producto['disponible']) ? 'Disponible' : 'No Disponible';  ?> </p>
+  </li>
+<?php
+}
 
 include 'includes/footer.php';
