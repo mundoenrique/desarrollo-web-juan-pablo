3c8e023f2caefef7be846c4976ddefc5e8dc07d0
mundoenrique <enrique.penaloza.working@gmail.com>
2024-11-12 14:22:09 -0400

    Funciones

--- 09-apache-php-mysql/15-funciones.php
+++ 09-apache-php-mysql/15-funciones.php
@@ -1,7 +1,13 @@
-<?php include 'includes/header.php';
+<?php
 
+declare(strict_types=1);
+include 'includes/header.php';
 
+function sumar(array $numero2, int $numero1 = 0)
+{
+  echo $numero1 + $numero2;
+}
 
-
+sumar([], 10);
 
 include 'includes/footer.php';
