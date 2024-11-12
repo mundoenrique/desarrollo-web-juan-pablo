0fc28f0f6ef923a143f7c1bce2568949eb62fd44
mundoenrique <enrique.penaloza.working@gmail.com>
2024-11-12 12:12:13 -0400

    Variables

--- 09-apache-php-mysql/02-variables.php
+++ 09-apache-php-mysql/02-variables.php
@@ -1,7 +1,30 @@
 <?php
 include_once 'includes/header.php';
 
+$nombre = 'Juan <br>';
+$_nombre = 'Enrique <br>';
+$nombre_ = 'Yajaira <br>';
 
+echo $nombre;
+var_dump($nombre);
 
+echo $_nombre;
+var_dump($_nombre);
+
+echo $nombre_;
+var_dump($nombre_);
+
+$nombre = 'María <br>';
+echo $nombre;
+var_dump($nombre);
+
+define('CONSTANTE', '<br>valor de la constante <br>');
+echo CONSTANTE;
+
+const CONSTANTE2 = 'otra constante<br>';
+echo CONSTANTE2;
+
+$nombreLCiente = 'Jesús';
+$nombre_cliente = 'Teresa';
 
 include_once 'includes/footer.php';
