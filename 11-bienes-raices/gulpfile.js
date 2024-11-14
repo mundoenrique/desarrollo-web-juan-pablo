cbc0ae329d7491042e827e68e0039bb25d7c5186
mundoenrique <enrique.penaloza.working@gmail.com>
2024-11-14 13:42:07 -0400

    tratamiento de imagenes

--- 11-bienes-raices/gulpfile.js
+++ 11-bienes-raices/gulpfile.js
@@ -1,14 +1,61 @@
 import fs from 'fs';
 import path from 'path';
+import sharp from 'sharp';
+import { optimize } from 'svgo';
+
 import * as sass from 'sass';
 import concat from 'gulp-concat';
-import sourcemaps from 'gulp-sourcemaps';
 import terser from 'gulp-terser';
+import sourcemaps from 'gulp-sourcemaps';
 import { dest, src, watch, series, parallel } from 'gulp';
 
 import { paths } from './gulp-help.js';
 
 const { img, js, scss } = paths;
+
+export async function imagesOptimize(done) {
+  const inputFolder = 'src/img';
+  const outputFolder = 'build/img/';
+  const width = 250;
+  const height = 180;
+
+  if (!fs.existsSync(outputFolder)) {
+    fs.mkdirSync(outputFolder, { recursive: true });
+  }
+
+  const images = fs.readdirSync(inputFolder).filter((file) => {
+    return /\.(jpg|jpeg|png)$/i.test(path.extname(file));
+  });
+
+  const svg = fs.readdirSync(inputFolder).filter((file) => {
+    return /\.(svg)$/i.test(path.extname(file));
+  });
+
+  try {
+    for (const file of images) {
+      const inputFile = path.join(inputFolder, file);
+      const outputFile = path.join(outputFolder, file);
+      console.log(`Procesando ${file}...`);
+      await sharp(inputFile).resize(width, height, { position: 'centre' }).toFile(outputFile);
+    }
+
+    svg.forEach((file) => {
+      const inputFile = path.join(inputFolder, file);
+      const outputFile = path.join(outputFolder, file);
+      const svgData = fs.readFileSync(inputFile, 'utf-8');
+      const result = optimize(svgData, { path: inputFile });
+      fs.writeFileSync(outputFile, result.data);
+      console.log(`Optimizado ${file}`);
+    });
+
+    console.log('Proceso completado.');
+
+    done();
+  } catch (error) {
+    console.error('Error al procesar las imágenes:', error);
+    done(error);
+  }
+}
 
 function buildCss(done) {
   try {
@@ -54,4 +101,4 @@ export function watchTask() {
   watch(js.src, buildJs);
 }
 
-export default series(buildCss, buildJs, parallel(watchTask));
+export default series(imagesOptimize, buildCss, buildJs, parallel(watchTask));
