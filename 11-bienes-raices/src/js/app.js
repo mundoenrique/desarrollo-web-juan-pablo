5cc6bf7696e57e78d54fd247bef7966caf5a7bc3
mundoenrique <enrique.penaloza.working@gmail.com>
2024-11-20 15:55:53 -0400

    Menu hamburgusa

--- 11-bienes-raices/src/js/app.js
+++ 11-bienes-raices/src/js/app.js
@@ -1,1 +1,15 @@
-console.log('hola');
+document.addEventListener('DOMContentLoaded', function () {
+  eventListeners();
+});
+
+function eventListeners() {
+  const movilMenu = document.querySelector('.mobile-menu');
+
+  movilMenu.addEventListener('click', navegacionResponsive);
+}
+
+function navegacionResponsive() {
+  const navegacion = document.querySelector('.navegacion');
+
+  navegacion.classList.toggle('mostrar');
+}
